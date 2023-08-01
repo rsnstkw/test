32e467bfbc71c97d215cf6b1d6a391b913d896dd
rsnstkw <rosen.stoykov@RSTOYKOV.speedy.bg>
2023-08-01 10:18:01 +0300

    Added hello blagovest

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
         echo "hello world!"
+        echo "HELLO BLAGOVEST"
         ?>
     </body>
 </html>
